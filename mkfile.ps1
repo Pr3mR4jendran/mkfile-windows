057f2d78036ab75f520626a20178ac857f1ac3f5
Prem Rajendran <75168548+Pr3mR4jendran@users.noreply.github.com>
2023-08-17 11:32:22 +0400

    Add files via upload

---
+++ mkfile.ps1
@@ -0,0 +1,45 @@
+param (
+    [string]$fname,
+    [int]$size
+)
+
+if (-not ($size -and $fname)) {
+    Write-Host ""
+    Write-Host "Usage: mkfile -size <size> -fname <fname>"
+    Write-Host ""
+    Write-Host ".SYNOPSIS"
+    Write-Host "Creates a dummy file with the specified size and name in the specified directory."
+    Write-Host ""
+    Write-Host ".DESCRIPTION"
+    Write-Host "This script creates a dummy file with random content of the specified size and name in the specified directory."
+    Write-Host ""
+    Write-Host ".PARAMETER size"
+    Write-Host "The size of the dummy file in bytes."
+    Write-Host ""
+    Write-Host ".PARAMETER path"
+    Write-Host "The path to the directory where the dummy file will be created."
+    Write-Host ""
+    Write-Host ".PARAMETER fname"
+    Write-Host "The name (with extension) of the dummy file."
+    Write-Host ""
+    Write-Host ".EXAMPLE"
+    Write-Host "mkfile -size 1048576 -fname mydummyfile.txt"
+    Write-Host ""
+    Write-Host "This example creates a dummy file named mydummyfile.txt with a size of 1048576 bytes in the specified directory."
+    Write-Host ""
+    exit
+}
+
+$out = New-Object byte[] $size
+(New-Object Random).NextBytes($out)
+
+$fileName = $fname
+$fullFilePath = Join-Path $PSScriptRoot $fileName
+
+[IO.File]::WriteAllBytes($fullFilePath, $out)
+
+if (Test-Path $fullFilePath) {
+    Write-Host ("Dummy file '$fullFilePath' successfully created")
+} else {
+    Write-Host "Failed to create dummy file"
+}
